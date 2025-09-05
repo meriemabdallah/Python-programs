065186026c5f9009ad08f8db0056eb0a98205213
Abdallah Meriem <meriembdl26@gmail.com>
2025-09-05 19:41:52 +0100

    Create print_output_without_newline.py

---
+++ print_output_without_newline.py
@@ -0,0 +1,12 @@
+# print output with new line
+
+print("meriem")
+print("is beautiful")
+
+# print output without new line
+
+print("meriem", end= " ")
+print("is beautiful")
+
+# the end parameter controls what will be printed at the end of the output *INSTEAD* of the " \n "
+# print("hello") == print("hello", end = "\n")
