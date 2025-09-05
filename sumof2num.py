fe811c1849d9e1dc737383478eea7e4d9542db34
Abdallah Meriem <meriembdl26@gmail.com>
2025-09-05 19:31:00 +0100

    Create sumof2num.py

---
+++ sumof2num.py
@@ -0,0 +1,12 @@
+# ask the user for 2 numbers and store them as variables
+
+a = input("enter the first number : ")
+b = input("enter the second number : ")
+
+# the addition of a and b
+
+z = float(a) + float(b)
+
+# print the sum of a and b 
+
+print(z)
